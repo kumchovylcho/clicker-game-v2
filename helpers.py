ef1629304643f6cd86b14e0cad44016112723f83
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-08-01 00:09:00 +0300

    added helpers

---
+++ helpers.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+def create_font(value:str, font:str, size_font:int, colour: tuple, bold=True, antialias=True):
+    font = pygame.font.SysFont(font, size_font, bold)
+    return font.render(value, antialias, colour)
+
+
+def calculate_center(main_obj_width, child_obj_width):
+    """
+    :return: int(pixels) of the X position where the child object must be placed
+    """
+    middle_of_main_obj = main_obj_width // 2
+    middle_of_child_obj = child_obj_width // 2
+
+    return middle_of_main_obj - middle_of_child_obj
