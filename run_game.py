eff0318d2f8b828f731a9a903629529a9cd481b0
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-08-04 00:17:50 +0300

    update the run_game.py

--- run_game.py
+++ run_game.py
@@ -3,6 +3,7 @@ from button import Button
 from coin_looper import CoinRotater
 from helpers import get_screen_size
 import settings
+from sound import Sound
 
 pg.init()
 
@@ -18,7 +19,15 @@ cursor = pg.image.load("images/cursor/cursor.png").convert_alpha()
 quit_button = Button((100, 40, 0), (180, 180, 180), (350, 400, 220, 70))
 quit_button.default_button_construction("Quit", "Georgia", 50, (255, 255, 255), True, True)
 settings_button = Button((100, 40, 0), (180, 180, 180), (350, 250, 220, 70))
-settings_button.default_button_construction("Settings", "Georgia", 50, (255, 255, 255), True, True)
+settings_button.default_button_construction("Start", "Georgia", 50, (255, 255, 255), True, True)
+sound_on = pg.image.load("images/sound/sound_on.png").convert_alpha()
+sound_off = pg.image.load("images/sound/sound_off.png").convert_alpha()
+sound_button = Sound(800, 700, sound_on, sound_off)
+
+pg.mixer.music.load('music/sound_of_menu/sound_for_menu.mp3')
+pg.mixer.music.play()
+pg.mixer.music.set_volume(settings.GAME_SOUND)
+
 
 coin_images = [pg.image.load(f"images/coins/coin_{i}.png").convert_alpha() for i in range(10)]
 coin_looper = CoinRotater(coin_x=10,
@@ -33,6 +42,12 @@ while game_running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game_running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            left_clicked = pg.mouse.get_pressed()[0]
+            if left_clicked:
+                sound_button.turn_off_on()
+        elif event.type == pg.MOUSEBUTTONUP:
+            pass
 
     screen.blit(background, (0, 0))
 
@@ -42,6 +57,8 @@ while game_running:
 
     settings_button.if_the_courser_is_above_the_button(mouse_position, screen)
     quit_button.if_the_courser_is_above_the_button(mouse_position, screen)
+
+    sound_button.display_image(screen)
 
     screen.blit(cursor, mouse_position)
     pg.display.update()
