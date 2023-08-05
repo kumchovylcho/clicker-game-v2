e3845ab0de54939c9d914abc3437badb274b111b
Tamer <tamerchoyy@gmail.com>
2023-08-06 00:57:13 +0300

    fixed a bug where the music is restarting when you click on random place on the screen

--- sound.py
+++ sound.py
@@ -1,10 +1,10 @@
-import pygame
+import pygame as pg
+import settings
 
 
-class Sound(pygame.sprite.Sprite):
+class Sound:
 
-    def __init__(self, x, y, sound_on, sound_off):
-        super(Sound, self).__init__()
+    def __init__(self, x: int, y: int, sound_on: pg.Surface, sound_off: pg.Surface):
         self.x_pos = x
         self.y_pos = y
         self.is_turned_on = True
@@ -12,6 +12,11 @@ class Sound(pygame.sprite.Sprite):
         self.sound_off = sound_off
         self.rect = sound_on.get_rect()
         self.rect.x, self.rect.y = self.x_pos, self.y_pos
+
+        self.sound_obj = pg.mixer.Sound('music/sound_of_menu/sound_for_menu.mp3')
+        self.menu_music = pg.mixer.Channel(0)
+        self.menu_music.play(self.sound_obj)
+        self.menu_music.set_volume(settings.GAME_SOUND)
 
     @property
     def get_image_position(self):
@@ -23,20 +28,24 @@ class Sound(pygame.sprite.Sprite):
     def get_image(self):
         if self.is_turned_on:
             return self.sound_on
-        else:
-            return self.sound_off
+        return self.sound_off
 
-    def draw(self):
-        pos = pygame.mouse.get_pos()
+    def check_button_state(self):
+        pos = pg.mouse.get_pos()
         if self.rect.collidepoint(pos):
-            if self.is_turned_on and pygame.mouse.get_pressed()[0]:
+            if self.is_turned_on:
                 self.is_turned_on = False
-            elif pygame.mouse.get_pressed()[0]:
+                self.pause_menu_music()
+
+            elif not self.is_turned_on:
                 self.is_turned_on = True
-        return self.is_turned_on
+                self.unpause_menu_music()
+
+    def pause_menu_music(self):
+        self.menu_music.pause()
+
+    def unpause_menu_music(self):
+        self.menu_music.unpause()
 
     def turn_off_on(self):
-        if self.draw():
-            pygame.mixer.music.play()
-        else:
-            pygame.mixer.music.pause()
+        self.check_button_state()
