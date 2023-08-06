085c341bdfb0fc44282d3ccef534c28cf74fd02a
Tamer <tamerchoyy@gmail.com>
2023-08-07 01:11:41 +0300

    update structure

--- map_handler/map.py
+++ map_handler/map.py
@@ -1,17 +1,19 @@
 from pygame import Surface
-from collections import deque
+from typing import Sequence
 
 from map_handler.monster import Monster
-from platform import Platform
+from map_handler.platform import Platform
 
 
 class Map:
 
     def __init__(self, background_img: Surface, platform: Platform):
         self.background_img = background_img
-        self.monsters = deque()
+        self.monsters = []
         self.platform = platform
+        self.current_monster_index = 0
 
-    def add_monster(self, monster: Monster):
-        self.monsters.appendleft(monster)
+    def add_monsters(self, monsters: Sequence[Monster]):
+        for monster in monsters:
+            self.monsters.append(monster)
 
