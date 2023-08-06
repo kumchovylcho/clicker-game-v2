4ad4f279fe1b0411ebe52b04bdc1100d211b3bc6
Tamer <tamerchoyy@gmail.com>
2023-08-07 01:12:36 +0300

    added init

--- map_handler/monster.py
+++ map_handler/monster.py
@@ -1,2 +1,10 @@
+from pygame import Surface
+
+
 class Monster:
-    pass
+
+    def __init__(self, image: Surface, health: int):
+        self.max_health = health
+        self.health = health
+        self.image = image
+
