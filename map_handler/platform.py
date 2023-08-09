b0d8986e3a8fe5bfc077cc969e7c6b28fd5bcccc
Tamer <tamerchoyy@gmail.com>
2023-08-09 23:32:07 +0300

    removed monster related stuff

--- map_handler/platform.py
+++ map_handler/platform.py
@@ -6,20 +6,11 @@ class Platform:
     def __init__(self,
                  img: Surface,
                  x_pos: int,
-                 y_pos: int,
-                 monster_x: int,
-                 monster_y: int):
+                 y_pos: int):
         self.img = img
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.monster_x = monster_x
-        self.monster_y = monster_y
 
     @property
     def get_position(self):
         return self.x_pos, self.y_pos
-
-    @property
-    def get_monster_position(self):
-        return self.monster_x, self.monster_y
-
