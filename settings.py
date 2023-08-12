b342410b744fbbc708467482832fca2c36c850a8
Tamer <tamerchoyy@gmail.com>
2023-08-12 20:39:05 +0300

    modified and added new constant values

--- settings.py
+++ settings.py
@@ -3,4 +3,9 @@ GAME_FPS = 60
 GAME_SOUND = 0.5
 
 monster_initial_health = 10
-monster_health_scale_value = 1.102
+monster_health_scale_value = 1.05
+last_monster_health = 0
+
+monster_initial_gold_reward = 1
+monster_initial_gold_reward_scale = 1.05
+last_monster_gold_reward = 0
