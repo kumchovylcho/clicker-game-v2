b432855ee2875acd23bf505d2e4800e218c66f15
petko <p.todorov1015@gmail.com>
2023-08-16 09:55:30 +0300

    menu and buttons fixed

--- button.py
+++ button.py
@@ -4,24 +4,40 @@ import pygame
 
 class Button:
 
-    def __init__(self, button_colour: tuple, if_above_color: tuple, rect_size:tuple):
+    def __init__(self, button_colour: tuple, if_above_color: tuple, rect_size: tuple):
         self.button_colour = button_colour
         self.if_above_color = if_above_color
         self.rect = pygame.Rect(*rect_size)
         self.surf_button = None
 
-    def check_if_hover(self, mouse_position:tuple) -> bool:
+    def check_if_hover(self, mouse_position: tuple) -> bool:
         return self.rect.collidepoint(mouse_position)
 
-    def default_button_construction(self, value:str, font:str, size_font:int, colour: tuple, bold=True, antialias=True):
+    def default_button_construction(self, value: str, font: str, size_font: int, colour: tuple, bold=True,
+                                    antialias=True):
         self.surf_button = create_font(value, font, size_font, colour, bold, antialias)
 
-    def if_the_courser_is_above_the_button(self, mouse_position:tuple, screen):
+    def if_the_courser_is_above_the_button(self, mouse_position: tuple, screen):
         colour_of_settings_button = self.button_colour
         if self.check_if_hover(mouse_position):
             colour_of_settings_button = self.if_above_color
 
-        pygame.draw.rect(screen, colour_of_settings_button, self.rect)
+        pygame.draw.rect(screen, colour_of_settings_button, self.rect, border_radius=30)
 
         calculate_x = calculate_center(self.rect.width, self.surf_button.get_width()) + self.rect.x
-        screen.blit(self.surf_button, (calculate_x, self.rect.y))
+        calculate_y = calculate_center(self.rect.height, self.surf_button.get_height()) + self.rect.y
+
+        screen.blit(self.surf_button, (calculate_x, calculate_y))
+
+    def check_collision(self, mouse_position):
+        return self.rect.collidepoint(mouse_position)
+
+    def center_button(self, screen_width, screen_height):
+        button_width = self.rect.width
+        button_height = self.rect.height
+
+        button_x = (screen_width - button_width) // 2
+        button_y = (screen_height - button_height) // 2
+
+        self.rect.topleft = (button_x, button_y)
+
--- menu.py
+++ menu.py
@@ -5,7 +5,11 @@ class Menu:
 
     def __init__(self):
         self.buttons: list = []
+        self.is_opened = True  # menu on/off
 
     def add_buttons(self, *buttons):
         for button in buttons:
             self.buttons.append(button)
+
+    def start_game(self):
+        self.is_opened = False
