2dd4a5bc93eb08abe1bd5271594ec367054edbc3
petko <p.todorov1015@gmail.com>
2023-08-16 09:54:29 +0300

    test run game

---
+++ rungametest.py
@@ -0,0 +1,117 @@
+import pygame as pg
+from helpers import get_screen_size
+
+pg.init()
+screen = pg.display.set_mode(get_screen_size())
+
+from button import Button
+from coin_looper import CoinRotater
+import settings
+from menu import Menu
+from player import Player
+from sound import Sound
+from map_handler.maps_controller import MapsController
+
+pg.mouse.set_visible(False)
+
+clock = pg.time.Clock()
+
+background = pg.image.load("images/menu/start_screen.jpg").convert_alpha()
+cursor = pg.image.load("images/cursor/cursor.png").convert_alpha()
+
+pg.display.set_icon(cursor)
+pg.display.set_caption("Clicker Forever")
+
+quit_button = Button((100, 0, 0), (180, 180, 180), (350, 400, 250, 90))
+quit_button.center_button(900, 800 + 150)
+quit_button.default_button_construction(
+    "Quit",
+    "Georgia",
+    50,
+    (255, 255, 255),
+    True,
+    True
+)
+settings_button = Button((0, 100, 0), (180, 180, 180), (350, 220, 250, 90))
+settings_button.center_button(900, 800 - 150)
+
+settings_button.default_button_construction(
+    "Start",
+    "Georgia",
+    50,
+    (255, 255, 255),
+    True,
+    True
+)
+
+sound_on = pg.image.load("images/sound/sound_on.png").convert_alpha()
+sound_off = pg.image.load("images/sound/sound_off.png").convert_alpha()
+sound_button = Sound(800, 700, sound_on, sound_off)
+
+menu = Menu()
+menu.add_buttons(quit_button, settings_button, sound_button)
+
+coin_images = [pg.image.load(f"images/coins/coin_{i}.png").convert_alpha() for i in range(10)]
+coin_looper = CoinRotater(coin_x=10,
+                          coin_y=10,
+                          images=coin_images
+                          )
+
+player = Player()
+maps_handler = MapsController(player)
+maps_handler.add_maps()
+maps_handler.set_monsters_initial_health_and_gold()
+
+game_running = True
+while game_running:
+    clock.tick(settings.GAME_FPS)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            game_running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            left_click = pg.mouse.get_pressed()[0]
+            mouse_pos = pg.mouse.get_pos()
+
+            if left_click:
+                sound_button.turn_off_on()
+
+            if not maps_handler.player.is_attacking and left_click and maps_handler.is_collide(mouse_pos):
+                maps_handler.attack_monster(mouse_pos=mouse_pos)
+                maps_handler.player.switch_attack_state()
+
+            if menu.is_opened and settings_button.check_collision(mouse_pos):
+                menu.is_opened = False
+
+            if menu.is_opened and quit_button.check_collision(mouse_pos):
+                game_running = False
+
+        elif event.type == pg.MOUSEBUTTONUP:
+            if maps_handler.player.is_attacking:
+                maps_handler.player.switch_attack_state()
+
+    screen.blit(background, (0, 0))
+    mouse_position = pg.mouse.get_pos()
+    sound_button.display_image(screen)
+
+    if menu.is_opened:
+        settings_button.if_the_courser_is_above_the_button(mouse_position, screen)
+        quit_button.if_the_courser_is_above_the_button(mouse_position, screen)
+        screen.blit(cursor, mouse_position)
+
+        pg.display.flip()
+        continue
+
+    maps_handler.display_map_with_platform_and_monster(screen=screen)
+    maps_handler.display_coin_animation(screen=screen)
+    maps_handler.display_float_damage(screen=screen)
+
+    coin_looper.rotate_coin(screen)
+
+    sound_button.display_image(screen)  # bug sled kato igrata startira butona izchezva
+
+    screen.blit(cursor, mouse_position)
+
+    pg.display.update()
+
+pg.quit()
