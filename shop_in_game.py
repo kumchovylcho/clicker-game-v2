4d51bf011dfab0ec06e7749ddc4f9efd572ffa71
Tamer <tamerchoyy@gmail.com>
2024-07-29 13:13:34 +0300

    removed recreation of surfaces and objects on every main loop iteration

--- shop_in_game.py
+++ shop_in_game.py
@@ -2,6 +2,8 @@ import math
 
 import pygame
 from helpers import create_font, numbers_format
+from text import Text
+import settings
 
 
 class Shop:
@@ -19,21 +21,28 @@ class Shop:
 
         self.companion_level = 0
 
-    @staticmethod
-    def draw_background(screen):
-        overlay = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
-        pygame.draw.rect(overlay, (0, 0, 0, 100), (0, 200, 350, 600), border_radius=15)
-        screen.blit(overlay, (0, 0))
+        self.menu_overlay = pygame.Surface(settings.SCREEN_SIZE, pygame.SRCALPHA)
 
-    @staticmethod
-    def draw_text(screen, x, y, text, color=(255, 255, 255)):
-        fnt = create_font(text, "Georgia", 20, color, True)
-        screen.blit(fnt, (x, y))
+        self.texts: [Text] = []
 
-    @staticmethod
-    def draw_text_companion(screen, x, y, text, color=(255, 255, 255)):
-        fnt = create_font(text, "Georgia", 27, color, True)
-        screen.blit(fnt, (x, y))
+    def add_texts(self, texts: list[Text]):
+        for text in texts:
+            self.texts.append(text)
+
+    def render_overlay(self, screen):
+        pygame.draw.rect(self.menu_overlay,
+                         settings.MENU_OVERLAY_COLOR,
+                         (0, 0, settings.MENU_OVERLAY_WIDTH, settings.MENU_OVERLAY_HEIGHT),
+                         border_radius=settings.MENU_OVERLAY_BORDER_RADIUS
+                         )
+        screen.blit(self.menu_overlay, settings.MENU_OVERLAY_POSITION)
+
+    def render_texts(self, screen):
+        for text in self.texts:
+            if text.text_id == settings.MENU_COMPANION_LEVEL_TEXT_ID and self.companion_level == 0:
+                continue
+
+            text.render_text(screen)
 
     def draw_button(self, screen, x, y, width, height, text, price):
         color = (160, 160, 160)
@@ -71,6 +80,31 @@ class Shop:
 
     def increase_click_price(self):
         self.click_power_price = math.ceil(self.click_power_price * 1.05)
+        self.click_power_level += 1
+        for text in self.texts:
+            if text.text_id != settings.MENU_DAMAGE_TEXT_ID:
+                continue
+
+            text.update_text(f"{settings.MENU_INCREASE_DAMAGE_TEXT}{self.click_power_level}")
 
     def increase_companion_price(self):
         self.companion_price = math.ceil(self.companion_price * 1.1)
+
+        for text in self.texts:
+            if text.text_id != settings.MENU_COMPANION_TEXT_ID:
+                continue
+
+            if text.text == settings.MENU_COMPANION_UPGRADE_TEXT:
+                break
+
+            text.update_text(settings.MENU_COMPANION_UPGRADE_TEXT)
+
+        self.increase_companion_level()
+
+    def increase_companion_level(self):
+        self.companion_level += 1
+        for text in self.texts:
+            if text.text_id != settings.MENU_COMPANION_LEVEL_TEXT_ID:
+                continue
+
+            text.update_text(f"{settings.MENU_COMPANION_LEVEL_TEXT}{self.companion_level}")
